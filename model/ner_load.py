c2be9567e040440e9a061eba5333fadeae396312
Kursov Mikhail <113849163+Blueredgray@users.noreply.github.com>
2025-08-05 18:02:59 +0300

    Update ner_load.py

--- model/ner_load.py
+++ model/ner_load.py
@@ -6,15 +6,14 @@ from loguru import logger
 def download_files():
     # Пример: файл "model.safetensors" из Google Drive
     file_url = "https://drive.google.com/drive/folders/1KqgJlybuhWlPsGu_UmRkCFWYXv3sILNk?usp=sharing"
-    #output_path = "model.safetensors"  # Или любой другой путь в текущей папке
-    output_folder = os.path.join(script_dir, "model")
+
+    # Путь к текущей папке (где лежит .py файл)
+    output_folder = os.path.dirname(os.path.abspath(__file__))
     
-    # Создаем папку, если она не существует
-    os.makedirs(output_folder, exist_ok=True)
-    
-    print(f"Скачиваю всю папку из Google Drive в {output_folder}...")
+    print(f"Скачиваю файлы в {output_folder}...")
     gdown.download_folder(file_url, output=output_folder, quiet=False)
 
 download_files()
-files_list = os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "model"))
+#files_list = os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "model"))
+files_list = os.listdir(os.path.dirname(os.path.abspath(__file__)))
 logger.info(f"Готово! Скачанные файлы: {files_list}")
