22304e1dfca4100207d9f5a26543fa34a82df872
Kursov Mikhail <113849163+Blueredgray@users.noreply.github.com>
2025-08-05 17:31:57 +0300

    Update ner_load.py

--- model/ner_load.py
+++ model/ner_load.py
@@ -13,15 +13,16 @@ def download_files():
     # Пример: файл "model.safetensors" из Google Drive
     file_url = "https://drive.google.com/drive/folders/1KqgJlybuhWlPsGu_UmRkCFWYXv3sILNk?usp=sharing"
     #output_path = "model.safetensors"  # Или любой другой путь в текущей папке
-    #output_path = "" 
+    output_folder = os.path.join(script_dir, "model")
     
-    #if not os.path.exists(output_path):
-    print("Скачиваю всю папку из Google Drive...")
-    gdown.download_folder(file_url, output=".", quiet=False)
-    #else:
-    #    print("Файлы уже загружены.")
+    # Создаем папку, если она не существует
+    os.makedirs(output_folder, exist_ok=True)
+    
+    print(f"Скачиваю всю папку из Google Drive в {output_folder}...")
+    gdown.download_folder(file_url, output=output_folder, quiet=False)
 
 if __name__ == "__main__":
     download_files()
-    print("Готово! Файлы в текущей папке:")
-    print(os.listdir())
+    print("Готово! Скачанные файлы:")
+    # Показываем содержимое папки model
+    print(os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")))
